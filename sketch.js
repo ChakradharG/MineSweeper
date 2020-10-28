9acd54d6d9c7a91a6657bafd9cd6d126307d51d3
DG <torreto279@live.com>
2020-10-28 10:19:22 +0530

    Gameover logic

--- sketch.js
+++ sketch.js
@@ -1,15 +1,16 @@
-let boxes = [],
+let tiles = [],
   rows = 10,
   cols = 10,
   width = 400,
   height = 400,
-  bombDensity = 0.2;
+  bombDensity = 0.2,
+  gameOver = false;
 
-function createBoxes() {
+function createTiles() {
   for (let y = 0; y < rows; y++) {
-    boxes[y] = [];
+    tiles[y] = [];
     for (let x = 0; x < cols; x++) {
-      boxes[y][x] = {
+      tiles[y][x] = {
         revealed: false,
         bomb: random(1) < bombDensity,
         neighCnt: 0
@@ -18,28 +19,33 @@ function createBoxes() {
   }
 }
 
-function countNeighbours(y, x) {
-  if (boxes[y][x].bomb) {
-    boxes[y][x].neighCnt = '💣';
-  } else {
-    for (let i = max(0, y - 1); i < min(rows, y + 2); i++) {
-      for (let j = max(0, x - 1); j < min(cols, x + 2); j++) {
-        if (boxes[i][j].bomb) {
-          boxes[y][x].neighCnt++;
+function countNeighbours() {
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      if (tiles[y][x].bomb) {
+        tiles[y][x].neighCnt = '💣';
+      } else {
+        for (let i = max(0, y - 1); i < min(rows, y + 2); i++) {
+          for (let j = max(0, x - 1); j < min(cols, x + 2); j++) {
+            if (tiles[i][j].bomb) {
+              tiles[y][x].neighCnt++;
+            }
+          }
         }
       }
+      dispTile(y, x);
     }
   }
 }
 
 function dispTile(y, x) {
-  if (boxes[y][x].revealed) {
+  if (tiles[y][x].revealed) {
     stroke(125);
     fill(255);
     rect(x * width / cols, y * height / rows, width / cols, height / rows);
     noStroke();
     fill(0);
-    text(boxes[y][x].neighCnt, (x + 0.5) * width / cols, (y + 0.5) * height / rows);
+    text(tiles[y][x].neighCnt, (x + 0.5) * width / cols, (y + 0.6) * height / rows);
   } else {
     stroke(125);
     fill(55);
@@ -48,29 +54,32 @@ function dispTile(y, x) {
 }
 
 function reveal() {
+  if (gameOver) {
+    return;
+  }
   y = parseInt(mouseY / 40);
   x = parseInt(mouseX / 40);
-  boxes[y][x].revealed = true;
+  tiles[y][x].revealed = true;
   dispTile(y, x);
+  if (tiles[y][x].neighCnt == '💣') {
+    gameOver = true;
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+        if (tiles[i][j].neighCnt == '💣') {
+          tiles[i][j].revealed = true;
+          dispTile(i, j);
+        }
+      }
+    }
+  }
 }
 
 function setup() {
   cnv = createCanvas(width, height);
   cnv.mousePressed(reveal);
   textAlign(CENTER);
-  createBoxes();
-  for (let y = 0; y < rows; y++) {
-    for (let x = 0; x < cols; x++) {
-      countNeighbours(y, x);
-      dispTile(y, x);
-    }
-  }
+  createTiles();
+  countNeighbours();
 }
 
-function draw() {
-  // for (let y = 0; y < rows; y++) {
-  //   for (let x = 0; x < cols; x++) {
-  //     dispTile(y, x);
-  //   }
-  // }
-}
+// function draw() {}
