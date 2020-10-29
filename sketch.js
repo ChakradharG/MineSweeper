a42cc4f0b39f7149e3f58d5c92e33e0aa444544a
DG <torreto279@live.com>
2020-10-29 15:40:44 +0530

    Win logic and neighbour reveal done

--- sketch.js
+++ sketch.js
@@ -1,9 +1,10 @@
 let tiles = [],
-  rows = 10,
-  cols = 10,
+  rows = 5,
+  cols = 5,
+  safeTiles = rows * cols,
   width = 400,
   height = 400,
-  bombDensity = 0.2,
+  bombDensity = 0.4,
   gameOver = false;
 
 function createTiles() {
@@ -13,17 +14,19 @@ function createTiles() {
       tiles[y][x] = {
         revealed: false,
         bomb: random(1) < bombDensity,
-        neighCnt: 0
+        neighCnt: 0,
+        flag: false
       };
     }
   }
 }
 
-function countNeighbours() {
+function configTiles() {
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
       if (tiles[y][x].bomb) {
         tiles[y][x].neighCnt = '💣';
+        safeTiles--;
       } else {
         for (let i = max(0, y - 1); i < min(rows, y + 2); i++) {
           for (let j = max(0, x - 1); j < min(cols, x + 2); j++) {
@@ -31,6 +34,9 @@ function countNeighbours() {
               tiles[y][x].neighCnt++;
             }
           }
+        }
+        if (tiles[y][x].neighCnt == 0) {
+          tiles[y][x].neighCnt = '';
         }
       }
       dispTile(y, x);
@@ -50,15 +56,31 @@ function dispTile(y, x) {
     stroke(125);
     fill(55);
     rect(x * width / cols, y * height / rows, width / cols, height / rows);
+    if (tiles[y][x].flag) {
+      fill(255, 64, 64);
+      noStroke();
+      triangle(x * width / cols + 20, y * height / rows + 20,
+              (x + 1) * width / cols - 20,y * height / rows + 20,
+              (x + 0.5) * width / cols, (y + 1) * height / rows - 20);
+    }
   }
 }
 
-function reveal() {
-  if (gameOver) {
+function pixToTile() {
+  y = parseInt(mouseY / (height / rows));
+  x = parseInt(mouseX / (width / cols));
+  if (mouseButton == LEFT) {
+    reveal(y, x);
+  } else if (mouseButton == RIGHT) {
+    tiles[y][x].flag = !tiles[y][x].flag;
+    dispTile(y, x);
+  }
+}
+
+function reveal(y, x) {
+  if (gameOver || tiles[y][x].revealed) {
     return;
   }
-  y = parseInt(mouseY / 40);
-  x = parseInt(mouseX / 40);
   tiles[y][x].revealed = true;
   dispTile(y, x);
   if (tiles[y][x].neighCnt == '💣') {
@@ -71,15 +93,26 @@ function reveal() {
         }
       }
     }
+    console.log('Lost');
+  } else if (--safeTiles == 0) {
+    gameOver = true;
+    console.log('Won');
+  } else if (tiles[y][x].neighCnt == '') {
+    for (let i = max(0, y - 1); i < min(rows, y + 2); i++) {
+      for (let j = max(0, x - 1); j < min(cols, x + 2); j++) {
+        if ((i != y || j != x) && !tiles[i][j].revealed) {
+          reveal(i, j);
+        }
+      }
+    }
   }
 }
 
 function setup() {
   cnv = createCanvas(width, height);
-  cnv.mousePressed(reveal);
+  cnv.mousePressed(pixToTile);
   textAlign(CENTER);
+  document.oncontextmenu = function() { return false; }
   createTiles();
-  countNeighbours();
+  configTiles();
 }
-
-// function draw() {}
