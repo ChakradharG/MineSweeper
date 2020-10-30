edb1853576686dd738e994cafcdf2c87d110227b
DG <torreto279@live.com>
2020-10-30 10:30:29 +0530

    Game logic done

--- sketch.js
+++ sketch.js
@@ -8,6 +8,7 @@ let tiles = [],
   gameOver = false;
 
 function createTiles() {
+  //Creates a 2D array of Tile objects of size rows*cols 
   for (let y = 0; y < rows; y++) {
     tiles[y] = [];
     for (let x = 0; x < cols; x++) {
@@ -22,6 +23,8 @@ function createTiles() {
 }
 
 function configTiles() {
+  /*Counts the bombs in the neighbouring tiles
+  for each tile, and displays the tiles on the screen*/
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
       if (tiles[y][x].bomb) {
@@ -35,7 +38,7 @@ function configTiles() {
             }
           }
         }
-        if (tiles[y][x].neighCnt == 0) {
+        if (tiles[y][x].neighCnt === 0) {
           tiles[y][x].neighCnt = '';
         }
       }
@@ -45,9 +48,10 @@ function configTiles() {
 }
 
 function dispTile(y, x) {
+  //Renders the tiles onto the screen, depending upon their state
   if (tiles[y][x].revealed) {
     stroke(125);
-    fill(255);
+    tiles[y][x].flag? fill(0,200,0) : fill(255);
     rect(x * width / cols, y * height / rows, width / cols, height / rows);
     noStroke();
     fill(0);
@@ -59,19 +63,20 @@ function dispTile(y, x) {
     if (tiles[y][x].flag) {
       fill(255, 64, 64);
       noStroke();
-      triangle(x * width / cols + 20, y * height / rows + 20,
-              (x + 1) * width / cols - 20,y * height / rows + 20,
-              (x + 0.5) * width / cols, (y + 1) * height / rows - 20);
+      triangle(x * width / cols + 25, y * height / rows + 30,
+              (x + 1) * width / cols - 25,y * height / rows + 30,
+              (x + 0.5) * width / cols, (y + 1) * height / rows - 25);
     }
   }
 }
 
 function pixToTile() {
+  //Converts the coordinates of the cursor into the index of the tile clicked
   y = parseInt(mouseY / (height / rows));
   x = parseInt(mouseX / (width / cols));
-  if (mouseButton == LEFT) {
+  if (mouseButton === LEFT) {
     reveal(y, x);
-  } else if (mouseButton == RIGHT) {
+  } else if (mouseButton === RIGHT) {
     tiles[y][x].flag = !tiles[y][x].flag;
     dispTile(y, x);
   }
@@ -83,21 +88,24 @@ function reveal(y, x) {
   }
   tiles[y][x].revealed = true;
   dispTile(y, x);
-  if (tiles[y][x].neighCnt == '💣') {
+  if (tiles[y][x].neighCnt === '💣') {
+    //If a bomb is clicked on, reveal all bombs and end the game
     gameOver = true;
     for (let i = 0; i < rows; i++) {
       for (let j = 0; j < cols; j++) {
-        if (tiles[i][j].neighCnt == '💣') {
+        if (tiles[i][j].neighCnt === '💣') {
           tiles[i][j].revealed = true;
           dispTile(i, j);
         }
       }
     }
     console.log('Lost');
-  } else if (--safeTiles == 0) {
+  } else if (--safeTiles === 0) {
+    //If all the non-bomb tiles have been revealed, end the game
     gameOver = true;
     console.log('Won');
-  } else if (tiles[y][x].neighCnt == '') {
+  } else if (tiles[y][x].neighCnt === '') {
+    //If the tile is empty, reveal all of its neighbours
     for (let i = max(0, y - 1); i < min(rows, y + 2); i++) {
       for (let j = max(0, x - 1); j < min(cols, x + 2); j++) {
         if ((i != y || j != x) && !tiles[i][j].revealed) {
@@ -112,7 +120,7 @@ function setup() {
   cnv = createCanvas(width, height);
   cnv.mousePressed(pixToTile);
   textAlign(CENTER);
-  document.oncontextmenu = function() { return false; }
+  document.oncontextmenu = function() { return false; } //To disable the browser context menu
   createTiles();
   configTiles();
 }
