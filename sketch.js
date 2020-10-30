5b7fc61c14d093402acb94501063659f82906300
DG <torreto279@live.com>
2020-10-30 16:56:24 +0530

    Minor changes

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 let tiles = [],
-  rows = 5,
-  cols = 5,
+  rows = 10,
+  cols = 10,
+  width = 800,
+  height = 800,
+  bombDensity = 0.2,
   safeTiles = rows * cols,
-  width = 400,
-  height = 400,
-  bombDensity = 0.4,
   gameOver = false;
 
 function createTiles() {
@@ -47,41 +47,6 @@ function configTiles() {
   }
 }
 
-function dispTile(y, x) {
-  //Renders the tiles onto the screen, depending upon their state
-  if (tiles[y][x].revealed) {
-    stroke(125);
-    tiles[y][x].flag? fill(0,200,0) : fill(255);
-    rect(x * width / cols, y * height / rows, width / cols, height / rows);
-    noStroke();
-    fill(0);
-    text(tiles[y][x].neighCnt, (x + 0.5) * width / cols, (y + 0.6) * height / rows);
-  } else {
-    stroke(125);
-    fill(55);
-    rect(x * width / cols, y * height / rows, width / cols, height / rows);
-    if (tiles[y][x].flag) {
-      fill(255, 64, 64);
-      noStroke();
-      triangle(x * width / cols + 25, y * height / rows + 30,
-              (x + 1) * width / cols - 25,y * height / rows + 30,
-              (x + 0.5) * width / cols, (y + 1) * height / rows - 25);
-    }
-  }
-}
-
-function pixToTile() {
-  //Converts the coordinates of the cursor into the index of the tile clicked
-  y = parseInt(mouseY / (height / rows));
-  x = parseInt(mouseX / (width / cols));
-  if (mouseButton === LEFT) {
-    reveal(y, x);
-  } else if (mouseButton === RIGHT) {
-    tiles[y][x].flag = !tiles[y][x].flag;
-    dispTile(y, x);
-  }
-}
-
 function reveal(y, x) {
   if (gameOver || tiles[y][x].revealed) {
     return;
@@ -99,11 +64,11 @@ function reveal(y, x) {
         }
       }
     }
-    console.log('Lost');
+    endScreen(false);
   } else if (--safeTiles === 0) {
     //If all the non-bomb tiles have been revealed, end the game
     gameOver = true;
-    console.log('Won');
+    endScreen(true);
   } else if (tiles[y][x].neighCnt === '') {
     //If the tile is empty, reveal all of its neighbours
     for (let i = max(0, y - 1); i < min(rows, y + 2); i++) {
@@ -116,11 +81,61 @@ function reveal(y, x) {
   }
 }
 
+function pixToTile() {
+  //Converts the coordinates of the cursor into the index of the tile clicked
+  if (mouseY > height || gameOver) {
+    return;
+  }
+  y = parseInt(mouseY / (height / rows));
+  x = parseInt(mouseX / (width / cols));
+  if (mouseButton === LEFT) {
+    reveal(y, x);
+  } else if (mouseButton === RIGHT) {
+    tiles[y][x].flag = !tiles[y][x].flag;
+    dispTile(y, x);
+  }
+}
+
+function dispTile(y, x) {
+  //Renders the tiles onto the screen, depending upon their state
+  if (tiles[y][x].revealed) {
+    stroke(125);
+    (tiles[y][x].flag && tiles[y][x].bomb) ? fill(94, 242, 19): fill(255);
+    rect(x * width / cols, y * height / rows, width / cols, height / rows);
+    noStroke();
+    fill(0);
+    text(tiles[y][x].neighCnt, (x + 0.5) * width / cols, (y + 0.6) * height / rows);
+  } else {
+    stroke(125);
+    fill(55);
+    rect(x * width / cols, y * height / rows, width / cols, height / rows);
+    if (tiles[y][x].flag) {
+      fill(255, 64, 64);
+      noStroke();
+      triangle(x * width / cols + 10, y * height / rows + 15,
+        (x + 1) * width / cols - 10, y * height / rows + 15,
+        (x + 0.5) * width / cols, (y + 1) * height / rows - 10);
+    }
+  }
+}
+
+function endScreen(res) {
+  textSize(30);
+  fill(255);
+  if (res) {
+    text('Victory!', width / 2, height + 50);
+  } else {
+    text('Defeat!', width / 2, height + 50);
+  }
+}
+
 function setup() {
-  cnv = createCanvas(width, height);
+  cnv = createCanvas(width, height + 50);
   cnv.mousePressed(pixToTile);
   textAlign(CENTER);
-  document.oncontextmenu = function() { return false; } //To disable the browser context menu
+  document.oncontextmenu = function() {
+    return false; //To disable the browser context menu
+  }
   createTiles();
   configTiles();
 }
